e8c502776a2314ef623b33ee7ac10f197e4d9503
egileekada <egileoniso.ekada@gmail.com>
2025-11-17 17:33:43 -0300

    Refactor PayoutTable component to integrate data fetching and loading state
    - Added `useFetchData` hook to fetch payout data dynamically.
    - Implemented `LoadingLayout` to handle loading states while data is being fetched.
    - Cleaned up the table structure for better readability and maintainability.

--- src/components/table/payoutTable.tsx
+++ src/components/table/payoutTable.tsx
@@ -1,48 +1,36 @@
+"use client"
+import { useFetchData } from "@/hook/useFetchData";
 import { Pagination, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@heroui/react";
+import { LoadingLayout } from "../shared";
 
 export default function PayoutTable() {
 
 
-    <tr>
-        <th className="text-left py-3 px-6 text-sm font-medium text-gray-600">
-            Name
-        </th>
-        <th className="text-left py-3 px-6 text-sm font-medium text-gray-600">
-            Available Balance
-        </th>
-        <th className="text-left py-3 px-6 text-sm font-medium text-gray-600">
-            Amount Requested
-        </th>
-        <th className="text-left py-3 px-6 text-sm font-medium text-gray-600">
-            Date
-        </th>
-        <th className="text-left py-3 px-6 text-sm font-medium text-gray-600">
-            Status
-        </th>
-        <th className="text-left py-3 px-6 text-sm font-medium text-gray-600">
-            Action
-        </th>
-    </tr>
+    const { data, isLoading } = useFetchData<any>({ name: "application", endpoint: "/payout/admin" });
+
+
     return (
-        <div className=" w-full flex flex-col gap-6 items-center " >
-            <Table aria-label="Example static collection table">
-                <TableHeader>
-                    <TableColumn>NAME</TableColumn>
-                    <TableColumn>Available Balance</TableColumn>
-                    <TableColumn>Amount Requested</TableColumn>
-                    <TableColumn>Date</TableColumn>
-                    <TableColumn>STATUS</TableColumn>
-                    <TableColumn>Action</TableColumn>
-                </TableHeader>
-                <TableBody>
-                    <TableRow key="1">
-                        <TableCell>Tony Reichert</TableCell>
-                        <TableCell>CEO</TableCell>
-                        <TableCell>Active</TableCell>
-                    </TableRow> 
-                </TableBody>
-            </Table>
-            <Pagination showControls initialPage={1} total={10} />
-        </div>
+        <LoadingLayout loading={isLoading} lenght={data?.item?.lenght} >
+            <div className=" w-full flex flex-col gap-6 items-center " >
+                <Table aria-label="Example static collection table">
+                    <TableHeader>
+                        <TableColumn>NAME</TableColumn>
+                        <TableColumn>Available Balance</TableColumn>
+                        <TableColumn>Amount Requested</TableColumn>
+                        <TableColumn>Date</TableColumn>
+                        <TableColumn>STATUS</TableColumn>
+                        <TableColumn>Action</TableColumn>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow key="1">
+                            <TableCell>Tony Reichert</TableCell>
+                            <TableCell>CEO</TableCell>
+                            <TableCell>Active</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+                <Pagination showControls initialPage={1} total={10} />
+            </div>
+        </LoadingLayout>
     )
 }
