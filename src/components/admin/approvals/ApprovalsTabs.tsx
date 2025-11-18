3952e8618aebcd83590257e0c360fb5a318c9be7
egileekada <egileoniso.ekada@gmail.com>
2025-11-18 13:18:51 -0300

    Refactor ApprovalsTabs component to remove commented-out code for improved readability
    - Cleaned up the `ApprovalsTabs` component by removing unnecessary commented-out navigation code, enhancing overall code clarity and maintainability.

--- src/components/admin/approvals/ApprovalsTabs.tsx
+++ src/components/admin/approvals/ApprovalsTabs.tsx
@@ -43,21 +43,7 @@ export default function ApprovalsTabs({
             {tabs.map((variant) => (
               <Tab key={variant.id} title={variant.label} />
             ))}
-          </Tabs>
-      {/* <nav className="flex space-x-8 px-6">
-        {tabs.map((tab) => (
-          <button
-            key={tab.id}
-            onClick={() => handleClick(tab?.id)}
-            className={`py-4 px-1 border-b-2 font-medium text-sm ${activeTab === tab.id
-                ? "border-blue-500 text-blue-600"
-                : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
-              }`}
-          >
-            {tab.label}
-          </button>
-        ))}
-      </nav> */}
+          </Tabs> 
       </div>
     </div>
   );
