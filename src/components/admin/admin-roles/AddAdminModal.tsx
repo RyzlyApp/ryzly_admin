39f96ef2d0cf9ecb190133ac9b5ed38f74d14a62
Neon <danielemmanuel257@gmail.com>
2026-05-17 17:28:14 +0100

    added functionality to add admin

--- src/components/admin/admin-roles/AddAdminModal.tsx
+++ src/components/admin/admin-roles/AddAdminModal.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import CustomButton from "@/components/custom/customButton";
 import {
   Modal,
@@ -15,6 +14,40 @@ import { FiUser } from "react-icons/fi";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 import { IoPeopleCircleOutline } from "react-icons/io5";
 import { LuUsersRound } from "react-icons/lu";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import httpService from "@/helper/services/httpService";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+export enum ADMIN_ROLE {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  GROWTH_MANAGER = "GROWTH_MANAGER",
+  PRODUCT_MANAGER = "PRODUCT_MANAGER",
+  COMMUNITY_SUPPORT = "COMMUNITY_SUPPORT",
+}
+
+type AddAdminFormValues = {
+  name: string;
+  email: string;
+  role: ADMIN_ROLE | "";
+  access: string[];
+};
+
+const addAdminSchema = yup.object({
+  fullName: yup.string().trim().required("Full name is required"),
+  email: yup
+    .string()
+    .trim()
+    .email("Enter a valid email")
+    .required("Email is required"),
+  role: yup
+    .mixed<ADMIN_ROLE>()
+    .oneOf(Object.values(ADMIN_ROLE) as ADMIN_ROLE[], "Select admin role")
+    .required("Role is required"),
+    password: yup.string().trim().required("Password is required"),
+});
 
 interface AddAdminModalProps {
   open: boolean;
@@ -77,20 +110,41 @@ export default function AddAdminModal({
   onClose,
   onSubmit,
 }: AddAdminModalProps) {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [role, setRole] = useState("");
-  const [access, setAccess] = useState<string[]>([
-    "dashboard",
-    "users",
-    "challenges",
-  ]);
+  const queryClient = useQueryClient();
 
-  const toggleAccess = (id: string) => {
-    setAccess((prev) =>
-      prev.includes(id) ? prev.filter((a) => a !== id) : [...prev, id]
-    );
-  };
+  const {
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      role: ADMIN_ROLE.ADMIN,
+      password: ''
+    },
+    mode: "onBlur",
+    resolver: yupResolver(addAdminSchema),
+  });
+
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: (data: { email: string; fullName: string; role: ADMIN_ROLE }) =>
+      httpService.post("/admin-auth/create-admin", data),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["admins"] });
+      onClose();
+      clearErrors();
+    },
+  });
+
+  const submit = async (data: any) => {
+    await mutateAsync(data);
+  }
 
   return (
     <Modal
@@ -102,40 +156,83 @@ export default function AddAdminModal({
       scrollBehavior="inside"
     >
       <ModalContent>
-        <ModalHeader className="flex items-center justify-between">
-          <h3 className="text-lg font-semibold">Add Admin</h3>
-        </ModalHeader>
-        <ModalBody className="space-y-2">
-          <div className="space-y-1">
-            <label className="text-sm text-gray-700">Full Name</label>
-            <input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Enter their full name"
-              className="w-full h-11 px-3 rounded-lg border border-gray-300 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div className="space-y-1">
-            <label className="text-sm text-gray-700">Email</label>
-            <input
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Enter email"
-              type="email"
-              className="w-full h-11 px-3 rounded-lg border border-gray-300 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-          <div className="space-y-1">
-            <label className="text-sm text-gray-700">Role</label>
-            <input
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
-              placeholder="What’s the admin’s role"
-              className="w-full h-11 px-3 rounded-lg border border-gray-300 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+        <form onSubmit={handleSubmit(submit)} className="contents">
+          <ModalHeader className="flex items-center justify-between">
+            <h3 className="text-lg font-semibold">Add Admin</h3>
+          </ModalHeader>
+          <ModalBody className="space-y-2">
+            <div className="space-y-1">
+              <label className="text-sm text-gray-700">Full Name</label>
+              <input
+                {...register("fullName")}
+                placeholder="Enter their full name"
+                className="w-full h-11 px-3 rounded-lg border border-gray-300 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              {errors.fullName?.message && (
+                <p className="text-xs text-red-600 font-medium">
+                  {errors.fullName.message}
+                </p>
+              )}
+            </div>
+            <div className="space-y-1">
+              <label className="text-sm text-gray-700">Email</label>
+              <input
+                {...register("email")}
+                placeholder="Enter email"
+                type="email"
+                className="w-full h-11 px-3 rounded-lg border border-gray-300 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              {errors.email?.message && (
+                <p className="text-xs text-red-600 font-medium">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+             <div className="space-y-1">
+              <label className="text-sm text-gray-700">Password</label>
+              <input
+                {...register("password")}
+                placeholder="Enter password"
+                type="password"
+                className="w-full h-11 px-3 rounded-lg border border-gray-300 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              {errors.password?.message && (
+                <p className="text-xs text-red-600 font-medium">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+            <div className="space-y-1">
+              <label className="text-sm text-gray-700">Role</label>
+              <select
+                {...register("role")}
+                className="w-full h-11 px-3 rounded-lg border border-gray-300 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"
+              >
+                <option value="" disabled>
+                  Select admin role
+                </option>
+                <option value={ADMIN_ROLE.SUPER_ADMIN}>
+                  {ADMIN_ROLE.SUPER_ADMIN}
+                </option>
+                <option value={ADMIN_ROLE.ADMIN}>{ADMIN_ROLE.ADMIN}</option>
+                <option value={ADMIN_ROLE.GROWTH_MANAGER}>
+                  {ADMIN_ROLE.GROWTH_MANAGER}
+                </option>
+                <option value={ADMIN_ROLE.PRODUCT_MANAGER}>
+                  {ADMIN_ROLE.PRODUCT_MANAGER}
+                </option>
+                <option value={ADMIN_ROLE.COMMUNITY_SUPPORT}>
+                  {ADMIN_ROLE.COMMUNITY_SUPPORT}
+                </option>
+              </select>
+              {errors.role?.message && (
+                <p className="text-xs text-red-600 font-medium">
+                  {errors.role.message}
+                </p>
+              )}
+            </div>
 
-          <div className="space-y-3">
+          {/* <div className="space-y-3">
             <p className="text-sm text-gray-600">Manage Access</p>
             <div className="space-y-3">
               {ACCESS_ITEMS.map((item) => (
@@ -156,25 +253,22 @@ export default function AddAdminModal({
                 </label>
               ))}
             </div>
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <div className="flex items-center justify-end gap-3 w-full">
-            <CustomButton variant="outline" onClick={onClose}>
-              Cancel
-            </CustomButton>
-            <CustomButton
-              variant="primary"
-              onClick={() => {
-                onSubmit({ name, email, role, access });
-                onClose();
-              }}
-            >
-              Add Admin
-            </CustomButton>
-          </div>
-        </ModalFooter>
+          </div> */}
+          </ModalBody>
+          <ModalFooter>
+            <div className="flex items-center justify-end gap-3 w-full">
+              <CustomButton variant="outline" type="button" onClick={onClose}>
+                Cancel
+              </CustomButton>
+              <CustomButton variant="primary" type="submit" isLoading={isPending}>
+                Add Admin
+              </CustomButton>
+            </div>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );
 }
+
+
